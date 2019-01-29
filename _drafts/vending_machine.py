bc8f592a5a74297a2bd0d7d340c17ad8633f40c6
aliwo <aliwo@naver.com>
2019-01-29 21:02:35 +0900

    포스팅 포스팅

--- _drafts/vending_machine.py
+++ _drafts/vending_machine.py
@@ -8,6 +8,35 @@
 #
 #
 # # number = int(input())
-eatable_ramen = ['진라면', '짜파게티']
-ramen = '짜파게티'
-print(ramen in eatable_ramen)
+
+
+class Noodle:  # 면 클래스 Noodle
+
+    def __init__(self):
+        self.ingredients = ['면']  # 모든 면 류 음식의 기본 재료는 '면
+
+
+class Zza(Noodle):  # 짜장면 클래스 Zza
+
+    def __init__(self):
+        Noodle.__init__(self)
+        self.ingredients.append('춘장')  # 짜장 = 면 + 춘장
+
+
+class Zzam(Noodle):  # 짬뽕 클래스 Zzam
+
+    def __init__(self):
+        Noodle.__init__(self)
+        self.ingredients.append('고추가루')  # 짬뽕 = 면 + 고추가루
+
+
+class ZzamZza(Zza, Noodle):  # 짬짜면 클래스 Zzam
+
+    def __init__(self):
+        Zza.__init__(self)
+        Zzam.__init__(self)
+
+my_dinner = ZzamZza()
+print(my_dinner.ingredients) # ['면', '고추가루'] 어 춘장 어디갔어
+
+
