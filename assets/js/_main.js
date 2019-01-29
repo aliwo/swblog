a06a6c5f85ac1f68fde36f2a5b91247bfa1ad499
aliwo <aliwo@naver.com>
2019-01-29 21:02:50 +0900

    Revert "decode URI 를 사용해 에스케이핑 된 id 값을 사용하는 smooth scroll 문제를 해결"
    This reverts commit fb9eb6c3775882e48bb20f9c3a5280d73577c8df.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
   var smoothScrolling = false;
   $(window).bind("popstate", function (event) {
     $.smoothScroll({
-      scrollTarget: decodeURI(location.hash),
+      scrollTarget: location.hash,
       offset: -20,
       beforeScroll: function() { smoothScrolling = true; },
       afterScroll: function() { smoothScrolling = false; }
@@ -74,7 +74,7 @@ $(document).ready(function() {
     }
   });
   // Smooth scroll on page load if there is a hash in the URL.
-  if (decodeURI(location.hash)) {
+  if (location.hash) {
     $(window).trigger("popstate");
   }
 
@@ -103,13 +103,13 @@ $(document).ready(function() {
       if (top <= scrollTop && scrollTop < bottom) {
         // Mark all ancestors as active
         links[i].link.parents("li").children("a").addClass('active');
-        if (links[i].href !== decodeURI(location.hash)) {
+        if (links[i].href !== location.hash) {
           history.replaceState(null, null, links[i].href);
         }
         return;
       }
     }
-    if ('#' !== decodeURI(location.hash)) {
+    if ('#' !== location.hash) {
       history.replaceState(null, null, '#');
     }
   }));
