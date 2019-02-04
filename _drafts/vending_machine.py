6ff844c98ddefb5804000095f17610a519512cb8
aliwo <aliwo@naver.com>
2019-02-04 17:07:27 +0900

    포스팅 포스팅

--- _drafts/vending_machine.py
+++ _drafts/vending_machine.py
@@ -1,42 +1,9 @@
-# # 자판기의 음료수 목록
-# merchandise = ['파워에이드', '코카콜라', '칠성사이다', '게토레이', '포카리스웨트', '스프라이트', '핫식스']
-#
-# # 마시고 싶은 음료수를 선택하자.
-#
-# print(merchandise[number])
-# print('를 자판기에서 뽑았다!')
-#
-#
-# # number = int(input())
+# vending_machine.py
+# 자판기의 음료수 목록
+merchandise = ['파워에이드', '코카콜라', '칠성사이다', '게토레이', '포카리스웨트', '스프라이트', '핫식스']
 
+# 마시고 싶은 음료수를 선택하자.
+number = int(input()) # 사용자는 0과 6사이에서 선택하면 된다.
 
-class Noodle:  # 면 클래스 Noodle
-
-    def __init__(self):
-        self.ingredients = ['면']  # 모든 면 류 음식의 기본 재료는 '면
-
-
-class Zza(Noodle):  # 짜장면 클래스 Zza
-
-    def __init__(self):
-        Noodle.__init__(self)
-        self.ingredients.append('춘장')  # 짜장 = 면 + 춘장
-
-
-class Zzam(Noodle):  # 짬뽕 클래스 Zzam
-
-    def __init__(self):
-        Noodle.__init__(self)
-        self.ingredients.append('고추가루')  # 짬뽕 = 면 + 고추가루
-
-
-class ZzamZza(Zza, Noodle):  # 짬짜면 클래스 Zzam
-
-    def __init__(self):
-        Zza.__init__(self)
-        Zzam.__init__(self)
-
-my_dinner = ZzamZza()
-print(my_dinner.ingredients) # ['면', '고추가루'] 어 춘장 어디갔어
-
-
+print(merchandise[number])
+print('를 자판기에서 뽑았다!')
--- series/principles_of_python/first_py.md
+++ series/principles_of_python/first_py.md
@@ -5,14 +5,21 @@ date:   2019-01-31 20:21:03 +0900
 
 
 ## Hello World!
-
+새로운 파일 hello.py 를 생성한 뒤, 다음과 같이 파일의 내용을 채워보자.
 {% highlight python %}
-
+print('hello world')
 {% endhighlight %}
-
+실행하면 화면에 hello world 가 출력된다면 성공이다. 여러분은 방금 파이썬을 사용해
+첫 프로그램을 실행 시킨 것이다.
 
 ## Hello GangNam Style
-
+화면에 hello world 만 출력되는 건 조금 심심하지 않은가?
+앞서 작성했던 내용은 지워버리자.
+<br><br>
+파이썬 강남스타일을 소개한다.
+아래의 내용을 복사해서 hello.py 안에 붙여넣자.
+그러면 파이썬이 연주하는 익숙한 강남 스타일 멜로디가 흘러나올 것이다.
+참고로 윈도우 사용자만 가능하니, mac 이나 리눅스 사용자에게는 양해를 구한다. 
 
 {% highlight python %}
 
@@ -65,6 +72,8 @@ winsound.Beep(369, 600) # F#
 # PYTHON GANGNAM STYLE!
 {% endhighlight %}
 
+파이썬을 사용하면 이처럼 강남스타일도 연주할 수 있다.
+이제 다음 장으로 넘어가자.
 
 파이썬 강남 스타일 링크
 <a target="_blank" href="https://codeboom.wordpress.com/2012/10/11/python-gangnam-style/">
